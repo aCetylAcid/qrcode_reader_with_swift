bf8376cff6f809a576cd5e16fc25b2f2eae3edd6
aCetylAcid <aka.l.m1ura.setsuna.karimofu@gmail.com>
2015-07-09 23:14:36 +0900

    ADD least functions

--- QRReaderWithSwift/ViewController.swift
+++ QRReaderWithSwift/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    // AVCaptureSession
+    let session:AVCaptureSession = AVCaptureSession()
+    
+    /* AVCaptureMetadataOutputObjectsDelegate */
+    func captureOutput(
+                captureOutput: AVCaptureOutput!,
+                didOutputMetadataObjects metadataObjects: [AnyObject]!,
+                fromConnection connection: AVCaptureConnection!) {
+        for m in metadataObjects {
+            if m.type == AVMetadataObjectTypeQRCode {
+                let qrcode = m.stringValue
+                println("QRcode: " + qrcode)
+            } else if m.type == AVMetadataObjectTypeEAN13Code {
+                let barcode = m.stringValue
+                println("Barcode: " + barcode)
+            }
+        }
+    }
 
+    /* lifecycle */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Camera devices
+        let devices:[AVCaptureDevice] = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+                                                           as! [AVCaptureDevice]
+        
+        // Use back camera
+        var targetDev:AVCaptureDevice?
+        for d in devices {
+            if d.position == AVCaptureDevicePosition.Back {
+                targetDev = d
+                break
+            }
+        }
+        
+        // Add Camera input to session.
+        var err:NSError? = nil
+        var input:AVCaptureInput? = nil
+        if let validDevice = targetDev {
+            input = AVCaptureDeviceInput.deviceInputWithDevice(validDevice, error: &err)
+                                                                as? AVCaptureInput
+        } else {
+            // ValidDevices(Cameras) not found
+            println("Error. there are no valid device(camera)s.")
+            return
+        }
+        self.session.addInput(input)
+        
+        // Add camera output to session
+        var output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        self.session.addOutput(output)
+        
+        // Get delegates for all metadata types.
+        output.metadataObjectTypes = output.availableMetadataObjectTypes;
+        
+        
+        // Start bar/qrcode detecting
+        self.session.startRunning()
+        
+        // Show preview on the screen
+        let previewLayer = AVCaptureVideoPreviewLayer.layerWithSession(self.session)
+                            as! AVCaptureVideoPreviewLayer
+        previewLayer.frame = self.view.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.addSublayer(previewLayer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
